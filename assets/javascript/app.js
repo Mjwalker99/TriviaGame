3860b7a7b1ed89aa83d402ffd669fd1a90d7b1fb
Micah Walker <minimelon19@gmail.com>
2018-09-12 13:53:27 -0500

    commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,163 @@
+$(document).ready(function () {
+
+    var questions = [{
+        ques: "Which MLB Team won the 2011 world series?",
+        ans: ["St. Louis Cardinals", "New York Yankees", "Boston Redsox", "Kansas city Royals"],
+        name: "Cardinals",
+        correct: "St. Louis Cardinals",
+        divClass: ".Cardinals"
+    },
+    {
+        ques: "Who was the NL MVP in 2017?",
+        ans: ["Joey Votto", "Paul Goldschmidt", "Giancarlo Stanton", "Nolan Arenado"],
+        name: "Stanton",
+        correct: "Giancarlo Stanton",
+        divClass: ".Stanton"
+    },
+    {
+        ques: "Who was the AL MVP IN 2017?",
+        ans: ["Aaron Judge", "Jose Altuve", "Mike Trout", "Francisco Lindor"],
+        name: "Altuve",
+        correct: "Jose Altuve",
+        divClass: ".Altuve"
+    },
+    {
+        ques: "Which MLB team has the most World Series titles?",
+        ans: ["Boston Redsox", "St. Louis Cardinals", "Pittsburg Pirates", "New York Yankees"],
+        name: "Yankees",
+        correct: "New York Yankees",
+        divClass: ".Yankees"
+    },
+    {
+        ques: "Which state has the most MLB teams?",
+        ans: ["Texas", "Florida", "California", "New York"],
+        name: "Cali",
+        correct: "California",
+        divClass: ".Cali"
+    },
+    {
+        ques: "To win a World Series a team has to win best of _ games?",
+        ans: ["7", "5", "9", "3"],
+        name: "Seven",
+        correct: "7",
+        divClass: ".Seven"
+    },
+
+    ]
+
+    var labels = ["first", "second", "third", "forth"];
+
+
+    var startGame = $("#start-btn").on('click', function () {
+        $(this).parent().hide();
+        $('.container').show();
+        countdown(60);
+        questionDisplay();
+    });
+
+
+    var questionDisplay = function () {
+        $(".questions :not('#sub-but')").empty();
+
+        for (var j = 0; j < 6; j++) {
+            $('.questions').prepend('<div class="' + questions[j].name + '"></div>');
+            $(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
+
+            for (var i = 0; i <= 3; i++) {
+                $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+            }
+            $('.questions').prepend('<hr />');
+        }
+    }
+
+    var countdown = function (seconds) {
+
+        var timer = setInterval(function () {
+            seconds = seconds - 1;
+            $("#time-remain").html(seconds);
+
+            if (seconds <= 0) {
+                $('.container').fadeOut(500);
+                var correctAnswers = 0;
+                var wrongAnswers = 0;
+                var unAnswered = 0;
+
+
+                for (var i = 0; i < 6; i++) {
+
+                    if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+                        correctAnswers++;
+                    }
+
+                    else if ($('input:radio[name="' + questions[i].name + '"]:checked').val() != questions[i].correct) {
+                        {
+
+                            wrongAnswers++;
+                        }
+                    }
+                    else {
+                        unAnswered++;
+                    }
+
+                }
+                $('#correctTimesUp').append(correctAnswers);
+
+                $('#wrongTimesUp').append(wrongAnswers);
+                $('#unansweredTimesUp').append(unAnswered);
+                $('#timesUp').fadeIn(1000).show();
+
+
+                clearInterval(timer);
+                return;
+            }
+        }, 1000);
+
+
+        $('#sub-but').on('click', function () {
+            clearInterval(timer);
+        })
+    };
+
+
+
+    var gradeQuiz = $('#sub-but').on('click', function () {
+
+        var correctAnswers = 0;
+        var wrongAnswers = 0;
+        var unAnswered = 0;
+
+
+        for (var i = 0; i < 6; i++) {
+
+            if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+                correctAnswers++;
+            }
+
+            else if ($('input:radio[name="' + questions[i].name + '"]:checked').val() != questions[i].correct) {
+                {
+
+                    wrongAnswers++;
+                }
+            }
+            else {
+                unAnswered++;
+            }
+        }
+
+
+        countdown();
+
+        $('.container').fadeOut(500);
+
+        $('#answerScreen').show();
+
+        $('#correctScreen').append(correctAnswers);
+
+        $('#wrongScreen').append(wrongAnswers);
+
+        $('#unanswered').append(unAnswered);
+
+    });
+});
